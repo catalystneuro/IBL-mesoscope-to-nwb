4a380453c6f18f31504283d12150b7660563800d
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-10-13 13:41:03 +0200

    Add IBLMesoscope segmentation extractor and interface implementation

---
+++ src/ibl_mesoscope_to_nwb/mesoscope2025/datainterfaces/__init__.py
@@ -0,0 +1,7 @@
+from ._ibl_suite2p_segmentation_extractor import IBLSuite2pSegmentationExtractor
+from ._ibl_suite2p_segmentation_interface import IBLSuite2pSegmentationInterface
+
+__all__ = [
+    "IBLSuite2pSegmentationExtractor",
+    "IBLSuite2pSegmentationInterface",
+]
